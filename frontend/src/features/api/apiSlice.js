eb425336f9fbdf5197c5ad39573405b6fb9dee81
Leytox <iladevder@gmail.com>
2024-08-21 23:45:20 +0300

    Changed logic for baseQuery

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -15,39 +15,44 @@ const baseQueryWithReauth = async (args, api, extraOptions) => {
 
   let result = await baseQuery(args, api, extraOptions);
   if (result.error && result.error.status === 401) {
-    // The token has expired
-    if (!mutex.isLocked()) {
-      const release = await mutex.acquire();
-      try {
-        // Attempt to refresh the token
-        const refreshResult = await baseQuery(
-          { url: "/auth/refresh", method: "POST" },
-          api,
-          extraOptions,
-        );
-        if (refreshResult.data) {
-          console.log(refreshResult.data);
-          // Retry the original request with new token
-          result = await baseQuery(args, api, extraOptions);
-          console.log(result);
-        } else {
-          // Refresh token has failed, log the user out or handle accordingly
-          api.dispatch(logout()); // Implement the logout action in your auth slice
+    // Check if there is an active user
+    const state = api.getState();
+    const user = state.auth.user; // Adjust this line based on your state structure
+
+    if (user) {
+      // The token has expired
+      if (!mutex.isLocked()) {
+        const release = await mutex.acquire();
+        try {
+          // Attempt to refresh the token
+          const refreshResult = await baseQuery(
+            { url: "/auth/refresh", method: "POST" },
+            api,
+            extraOptions,
+          );
+          if (refreshResult.data) {
+            console.log(refreshResult.data);
+            // Retry the original request with new token
+            result = await baseQuery(args, api, extraOptions);
+            console.log(result);
+          } else {
+            // Refresh token has failed, log the user out or handle accordingly
+            if (user) api.dispatch(logout()); // Implement the logout action in your auth slice
+          }
+        } finally {
+          // Release the mutex regardless of success or failure
+          release();
         }
-      } finally {
-        // Release the mutex regardless of success or failure
-        release();
+      } else {
+        // Wait until the mutex is released before retrying the original request
+        await mutex.waitForUnlock();
+        result = await baseQuery(args, api, extraOptions);
       }
-    } else {
-      // Wait until the mutex is released before retrying the original request
-      await mutex.waitForUnlock();
-      result = await baseQuery(args, api, extraOptions);
     }
   }
 
   return result;
 };
-
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: baseQueryWithReauth,
